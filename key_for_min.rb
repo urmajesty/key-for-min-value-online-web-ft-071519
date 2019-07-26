8f06c51d3e043995f195ebc875629d770a9f4de9
Chyrelle Lewis <chyrellelewis@gmail.com>
2019-07-26 17:44:01 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,7 @@ def key_for_min_value(name_hash)
     low_key = nil 
   low_value = nil
   name_hash.each do |k, v|
-    if no_key == nil || v < low_value
+    if low_key == nil || v < low_value
       low_value = v 
       low_key = k 
     end 
